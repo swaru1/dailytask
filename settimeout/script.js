8a16d8bcf924380168bd1fadf759e573a4fd8190
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-10-08 20:16:19 +0530

    progess bar

---
+++ settimeout/script.js
@@ -0,0 +1,25 @@
+var h4 = document.querySelector("h4");
+var h3 = document.querySelector("h3");
+var button = document.querySelector("button");
+var progress = document.querySelector(".progress");
+var bar = document.querySelector(".p-bar");
+
+button.addEventListener("click", function (dets) {
+  var p = 0;
+  button.disabled = true;
+  button.innerHTML = "Downloading...";
+  h3.innerHTML = "Please wait..";
+
+  var time = setInterval(() => {
+    p++;
+    progress.style.width = `${p}%`;
+    h4.textContent = `${p}%`;
+
+    if (p >= 100) {
+      clearInterval(time);
+      h3.innerHTML = "Download Completed!";
+      button.innerHTML = "Finish";
+      button.disabled = false;
+    }
+  }, 50);
+});
